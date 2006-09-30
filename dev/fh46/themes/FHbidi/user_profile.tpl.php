05655eee31109d84536092acad9ef031b71c2cc3
quinto <quinto@f2aa1e37-6e3e-0410-a7f5-67f85424188b>
2006-09-30 14:58:01 +0000

    test 4 post-commit 5

--- dev/fh46/themes/FHbidi/user_profile.tpl.php
+++ dev/fh46/themes/FHbidi/user_profile.tpl.php
@@ -76,7 +76,6 @@ Walk with us!</p>
 //  print _donation_list($user->donation);
 //  print "<hr />";
 //  foreach($fields as $key => $value) {
-
 //  foreach($user as $key => $value) {
 //    print "<h3>$key</h3>";
 //    print "<p>" . htmlentities(print_r($value,true)) . "</p>";

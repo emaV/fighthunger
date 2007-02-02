de4f5f819c9fdf7d23973194fcf95311219a2b78
quinto <quinto@f2aa1e37-6e3e-0410-a7f5-67f85424188b>
2007-02-02 12:18:03 +0000

    fhwalk theme FIX 3

--- dev/fh47/themes/FHBidi/node-fhwalk.tpl.php
+++ dev/fh47/themes/FHBidi/node-fhwalk.tpl.php
@@ -51,48 +51,9 @@
 // Footer tabs
   print $node->tabs['footer'];
   print "<div style='clear:both;'>&nbsp;</div>";
- 
-  
-// DEBUG
-/*
-  print "\n\n<!-- DEBUG\n\n";
-    
-  $hook_array = $node->hook_view;
-  foreach($node->hook_view as $key => $value) {
-   print "#### \n\n";
-   print "#### $key\n\n";
-   print "############################################\n";
-   print htmlentities($value) . "\n\n";
-   print "############################################\n";
-  }
- 
-  print "\n\n\n\n" . htmlentities(print_r($node, true));
-
-  print "\n\nEND DEBUG -->\n\n";
-*/
-
 ?>
   </div>
-<?php if ($links): ?>
-  <div class="links"><?php // print $links ?></div>
-  <div class="terms"></div>
-<?php endif; ?>
-
 
 </div>
-
-
-
-<?php
-/*
-$alias = db_result(db_query("SELECT dst FROM {url_alias} WHERE src='node/%d'",$node['nid']));
-list($campaign, $event) = split('/',  $alias );
-$mail_to = "$event.$campaign@fighthunger.org";
-$subject = 'subject=' . utf8_encode("tags: $campaign $event");
-//$body = 
-// print ("<a href='mailto:$mail_to?$subject'>$mail_to</a>");
-// print("<br />alias: $alias");
-?>
-
 
 <!-- fhwalk end -->

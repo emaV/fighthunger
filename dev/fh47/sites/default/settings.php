431d440715c21a4e1332cfe6d9c0e8304a245977
jose <jose@f2aa1e37-6e3e-0410-a7f5-67f85424188b>
2007-02-04 14:16:14 +0000

    Drupal 4.7.6 sites/default/settings.php manually updated

--- dev/fh47/sites/default/settings.php
+++ dev/fh47/sites/default/settings.php
@@ -135,6 +135,19 @@ ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
 
 /**
+ * We try to set the correct cookie domain. If you are experiencing problems
+ * try commenting out the code below or specifying the cookie domain by hand.
+ */
+if (isset($_SERVER['HTTP_HOST'])) {
+  $domain = '.'. preg_replace('`^www.`', '', $_SERVER['HTTP_HOST']);
+  // Per RFC 2109, cookie domains must contain at least one dot other than the
+  // first. For hosts such as 'localhost', we don't set a cookie domain.
+  if (count(explode('.', $domain)) > 2) {
+    ini_set('session.cookie_domain', $domain);
+  }
+}
+
+/**
  * Variable overrides:
  *
  * To override specific entries in the 'variable' table for this site,

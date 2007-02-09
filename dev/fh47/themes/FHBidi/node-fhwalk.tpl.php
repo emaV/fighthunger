a2c300ac18236616720216f28b1bbdfc58772fdf
quinto <quinto@f2aa1e37-6e3e-0410-a7f5-67f85424188b>
2007-02-09 12:14:16 +0000

    walk theming preview 2

--- dev/fh47/themes/FHBidi/node-fhwalk.tpl.php
+++ dev/fh47/themes/FHBidi/node-fhwalk.tpl.php
@@ -1,9 +1,56 @@
 <!-- fhwalk start -->
 <div class="node<?php print ($sticky) ? " sticky" : ""; ?>">
 
-  <?php if ($page == 0): ?>
-    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-  <?php endif; ?>
+  <?php if ($page == 0): //teaser view ?>
+  
+    <?php if ($node->in_preview): //preview  ?>
+      <h2><?php print $title ?></h2>
+      
+    <?php else: //normal ?>
+      <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+
+    <?php endif; ?>
+
+  <div class="content">
+<?php
+
+// When
+  print "<h3>" . t("When") . "</h3>";
+  print $node->hook_view['event'];
+// Where
+  print $node->hook_view['location'];
+
+// Other Details
+  if($node->location['additional']<>'') {
+    print "<h3>" . t("Directions") . "</h3>";
+    print $node->location['additional'];
+  }
+  if($node->route<>'') {
+    print '<h3>' . t('Route') . '</h3>'; 
+    print $node->route;
+  }
+  if($node->body_plain) {
+    print '<h3>' . t('Walk Information') . '</h3>'; 
+    print $node->body_plain;
+  }
+  
+// Signup
+  if ($node->in_preview) {
+    print '<h3>' . t('Signup settings') . '</h3>';
+    print '<ul>'; 
+    print '<li>' . t('Signup Approval') . ': <strong>' . _fhsignup_status($node->signup_status) . '</strong></li>'; 
+    print '<li>' . t('Show signups') . ': <strong>'        . (($node->signup_show) ? t('yes') : t('no')) . '</strong></li>'; 
+    print '<li>' . t('Show signups number') . ': <strong>' . (($node->signup_show_number) ? t('yes') : t('no')) . '</strong></li>'; 
+    print '<li>' . t('Show user comments') . ': <strong>'  . (($node->signup_show_comments) ? t('yes') : t('no')) . '</strong></li>'; 
+    print '<li>' . t('Group Signup') . ': <strong>'        . (($node->signup_allow_guest) ? t('yes') : t('no')) . '</strong></li>';
+    print '</ul>';
+  }
+
+?>
+
+  </div>
+
+  <?php else: //full node view ?>
 
   <div class="content">
 <?php
@@ -53,7 +100,8 @@
   print "<div style='clear:both;'>&nbsp;</div>";
 ?>
   </div>
+  
+  <?php endif; ?>
 
 </div>
-
 <!-- fhwalk end -->
